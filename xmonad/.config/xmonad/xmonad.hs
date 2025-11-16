85c98f0727aaccdd175d64d2ddfd2e07cae04cc8
behdad <behdadbabaie@gmail.com>
2025-11-16 14:30:43 +0000

    adding xmonad and xmobar config files

---
+++ xmonad/.config/xmonad/xmonad.hs
@@ -0,0 +1,86 @@
+import XMonad
+import XMonad.Util.EZConfig
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.Magnifier
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+import XMonad.Util.Loggers
+import XMonad.ManageHook
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.Renamed
+
+
+
+
+
+main :: IO ()
+main = xmonad
+     . ewmhFullscreen 
+     . ewmh 
+     . withEasySB (statusBarProp "xmobar ~/.config/xmobar/xmobarrc" (pure myXmobarPP)) defToggleStrutsKey
+     $ myConfig
+  where
+    toggleStrutsKey :: XConfig Layout -> (KeyMask, KeySym)
+    toggleStrutsKey XConfig{ modMask = m } = (m, xK_z)
+
+myConfig = def 
+    {terminal           = "alacritty"
+    , modMask           = mod4Mask     -- Rebind Mod to the Super/Windows key
+    , layoutHook        = myLayout     -- Use cudtom layouts
+    , manageHook        = myManageHook -- Match on certain windows
+    , borderWidth       = 2
+    , focusFollowsMouse = True
+    , focusedBorderColor  = "#2a6dc9"
+    }
+    `additionalKeysP` 
+     [ ("M-S-b" , spawn "brave")
+     ]
+
+myManageHook :: ManageHook
+myManageHook = composeAll
+    [ isDialog   --> doFloat ]
+
+
+
+myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
+  where
+    threeCol
+        = renamed [Replace "ThreeCol"]
+        $ magnifiercz' 1.3
+        $ ThreeColMid nmaster delta ratio
+    tiled   = Tall nmaster delta ratio
+    nmaster = 1      -- Default number of windows in the master pane
+    ratio   = 1/2    -- Default proportion of screen occupied by master pane
+    delta   = 3/100  -- Percent of screen to increment by when resizing panes
+
+
+myXmobarPP :: PP
+myXmobarPP = def
+    { ppSep             = magenta " • "
+    , ppTitleSanitize   = xmobarStrip
+    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+    , ppHidden          = white . wrap " " ""
+    , ppHiddenNoWindows = lowWhite . wrap " " ""
+    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+    , ppExtras          = [logTitles formatFocused formatUnfocused]
+    }
+  where
+    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
+    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
+
+    -- | Windows should have *some* title, which should not not exceed a
+    -- sane length.
+    ppWindow :: String -> String
+    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
+
+    blue, lowWhite, magenta, red, white, yellow :: String -> String
+    magenta  = xmobarColor "#ff79c6" ""
+    blue     = xmobarColor "#bd93f9" ""
+    white    = xmobarColor "#f8f8f2" ""
+    yellow   = xmobarColor "#f1fa8c" ""
+    red      = xmobarColor "#ff5555" ""
+    lowWhite = xmobarColor "#bbbbbb" ""
+
